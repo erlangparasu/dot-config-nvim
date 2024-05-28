c79c108c65a2a3daa78fa46477e457cc6c6b9523
Erlang Parasu <erlangparasu@gmail.com>
2024-05-28 12:16:46 +0800

    chore: check vscode and neovide

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+    -- require("config.lazy")
+elseif vim.g.neovide then
+    require("config.lazy")
+else
+    require("config.lazy")
+end
