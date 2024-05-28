357a2c3f9808bcc40baaef91b0629c359997c3f4
Erlang Parasu <erlangparasu@gmail.com>
2024-05-28 15:40:03 +0800

    feat: adjust update concurrency

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -63,7 +63,7 @@ require("lazy").setup({
   checker = {
     -- automatically check for plugin updates
     enabled = true,
-    concurrency = nil, ---@type number? set to 1 to check for updates very slowly
+    concurrency = 8, ---@type number? set to 1 to check for updates very slowly
     notify = true, -- get a notification when new updates are found
     frequency = 86400, -- check for updates every 24 hours
     -- check_pinned = false, -- check for pinned packages that can't be updated
