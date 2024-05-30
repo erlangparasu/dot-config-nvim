9730c9e001e59f8b737925b389ed75eca967b21e
Erlang Parasu <erlangparasu@gmail.com>
2024-05-30 16:59:50 +0800

    feat: enable copy paste in Neovide using system clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+-- opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
+opt.clipboard='unnamedplus'
