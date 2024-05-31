6c32d949a48f6f1fc49398b4fe23a090bba95987
Erlang Parasu <erlangparasu@gmail.com>
2024-05-31 11:34:51 +0800

    fix: resize window keymaps on macos

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local map = LazyVim.safe_keymap_set
+
 if vim.g.neovide then
     -- NOTE: https://github.com/neovide/neovide/issues/1282#issuecomment-2106304616
     -- NOTE: Ctrl+Shift+C to Copy, Ctrl+Shitf+V to paste
@@ -11,4 +13,15 @@ if vim.g.neovide then
 	vim.api.nvim_set_keymap('c', '<sc-v>', '<C-o>l<C-o>"+<C-o>P<C-o>l', {noremap = true})
 	vim.api.nvim_set_keymap('i', '<sc-v>', '<ESC>l"+Pli', {noremap = true})
 	vim.api.nvim_set_keymap('t', '<sc-v>', '<C-\\><C-n>"+Pi', {noremap = true})
+end
+
+-- Resize window using <ctrl> arrow keys
+-- Reference:
+-- - https://github.com/LazyVim/LazyVim/blob/060f56d6d429c5ee3fd35f4fa0c152f4a42781ca/lua/lazyvim/config/keymaps.lua#L20
+-- - https://github.com/LunarVim/LunarVim/pull/725/files
+if vim.fn.has("mac") then
+	map("n", "<A-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+	map("n", "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+	map("n", "<A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+	map("n", "<A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 end
