b526acff30a8123465cb561d7cde21cf435d9e1f
Erlang Parasu <erlangparasu@gmail.com>
2024-05-31 14:48:57 +0800

    example neovide cursor effect

--- init.lua
+++ init.lua
@@ -2,6 +2,9 @@
 if vim.g.vscode then
     -- require("config.lazy")
 elseif vim.g.neovide then
+    -- vim.g.neovide_cursor_vfx_mode = "pixiedust"
+    -- vim.g.neovide_cursor_vfx_mode = "wireframe"
+
     require("config.lazy")
 else
     require("config.lazy")
