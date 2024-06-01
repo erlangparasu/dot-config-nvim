a074aa360d2bd9ba799cabef5582dbcdc4e968dd
Erlang Parasu <erlangparasu@gmail.com>
2024-06-02 00:50:11 +0800

    fix: open nvim-tree on load file buffer

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -10,12 +10,21 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
-vim.api.nvim_create_autocmd({"BufNewFile", "BufReadPost"}, {
-  callback = function(args)
-    if vim.fn.expand "%:p" ~= "" then
-      vim.api.nvim_del_autocmd(args.id)
-      vim.cmd "noautocmd NvimTreeOpen"
-      vim.cmd "noautocmd wincmd p"
-    end
-  end,
-})
+-- NOTE: Ref: https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup#open-for-files-and-no-name-buffers
+local function open_nvim_tree(args)
+  -- buffer is a real file on the disk
+  local real_file = vim.fn.filereadable(args.file) == 1
+
+  -- buffer is a [No Name]
+  local no_name = args.file == "" and vim.bo[args.buf].buftype == ""
+
+  if not real_file and not no_name then
+    return
+  end
+
+  -- open the tree, find the file but don't focus it
+  require("nvim-tree.api").tree.open({ focus = false, find_file = true })
+end
+
+-- NOTE: Ref: https://neovim.io/doc/user/autocmd#autocmd-events
+vim.api.nvim_create_autocmd({ "VimEnter", "BufNewFile", "BufRead", "BufReadPost", "SessionLoadPost" }, { callback = open_nvim_tree })
