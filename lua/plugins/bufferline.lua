0590e84274cb36d619cf17a4536bb9f9ac1f76a5
Erlang Parasu <erlangparasu@gmail.com>
2024-06-09 17:12:36 +0800

    fix: config bufferline for nvim-tree

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        -- stylua: ignore
+        close_command = function(n) LazyVim.ui.bufremove(n) end,
+        -- stylua: ignore
+        right_mouse_command = function(n) LazyVim.ui.bufremove(n) end,
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = true,
+        diagnostics_indicator = function(_, _, diag)
+          local icons = require("lazyvim.config").icons.diagnostics
+          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+            .. (diag.warning and icons.Warn .. diag.warning or "")
+          return vim.trim(ret)
+        end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            highlight = "Directory",
+            separator = true, -- use a "true" to enable the default, or set your own character
+            text_align = "left",
+          },
+        },
+        ---@param opts bufferline.IconFetcherOpts
+        get_element_icon = function(opts)
+          return LazyVim.config.icons.ft[opts.filetype]
+        end,
+      },
+    },
+  }
+}
