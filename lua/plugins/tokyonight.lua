ed32d95689717cbea6f26420ab00a7613c831658
Erlang Parasu <erlangparasu@gmail.com>
2024-06-10 16:27:29 +0800

    chore: theme style

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,7 +1,7 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { style = "storm" },
-  }
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = true,
+  --   opts = { style = "storm" },
+  -- }
 }
