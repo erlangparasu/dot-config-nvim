228e21f6e232b71d72eda82814af8d1dd518dc9a
Erlang Parasu <erlangparasu@gmail.com>
2024-06-17 09:56:00 +0800

    feat: add cmp snippets

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,18 +1,28 @@
 return {
   -- auto completion
-  {
-    "golang/vscode-go",
-  },
-  {
-    "hrsh7th/vim-vsnip",
-  },
-  {
-    "hrsh7th/vim-vsnip-integ",
-  },
+  { "golang/vscode-go" },
+  { "Neevash/awesome-flutter-snippets" },
+  { "stevearc/vim-vscode-snippets" },
+  --
+  { "hrsh7th/vim-vsnip" },
+  { "hrsh7th/vim-vsnip-integ" },
+  --
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-nvim-lua" },
+  { "hrsh7th/cmp-nvim-lsp-signature-help" },
+  { "hrsh7th/cmp-vsnip" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-buffer" },
+  --
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       table.insert(opts.sources, { name = "vsnip", group_index = 0 })
+      table.insert(opts.sources, { name = "nvim_lsp" })
+      table.insert(opts.sources, { name = "nvim_lsp_signature_help" })
+      table.insert(opts.sources, { name = "nvim_lua" })
+      table.insert(opts.sources, { name = "buffer" })
+      table.insert(opts.sources, { name = "path" })
     end,
   },
 }
