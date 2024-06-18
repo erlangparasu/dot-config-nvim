b63b3339138782334b6cb7c013879b7a4247d0ca
Erlang Parasu <erlangparasu@gmail.com>
2024-06-18 16:09:55 +0800

    feat: disable inlay_hints

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -58,7 +58,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.project" },
     -- import/override with your plugins
     { import = "plugins" },
-    --
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
