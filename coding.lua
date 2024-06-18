abadb1c74ed69f92e2375a7ef4901bdfaa57425c
Erlang Parasu <erlangparasu@gmail.com>
2024-06-18 16:10:30 +0800

    wip: coding suggestions

--- coding.lua
+++ coding.lua
@@ -9,7 +9,7 @@ return {
   --
   { "hrsh7th/cmp-nvim-lsp" },
   { "hrsh7th/cmp-nvim-lua" },
-  { "hrsh7th/cmp-nvim-lsp-signature-help" },
+  -- { "hrsh7th/cmp-nvim-lsp-signature-help" },
   { "hrsh7th/cmp-vsnip" },
   { "hrsh7th/cmp-path" },
   { "hrsh7th/cmp-buffer" },
@@ -18,7 +18,7 @@ return {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       table.insert(opts.sources, { name = "nvim_lsp" })
-      table.insert(opts.sources, { name = "nvim_lsp_signature_help" })
+      -- table.insert(opts.sources, { name = "nvim_lsp_signature_help" })
       table.insert(opts.sources, { name = "buffer" })
       table.insert(opts.sources, { name = "path" })
       table.insert(opts.sources, { name = "vsnip" })
