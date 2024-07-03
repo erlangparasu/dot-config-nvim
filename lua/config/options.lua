f607a2385be5f0021fb68a9b1355f3ec1172796d
Erlang Parasu <erlangparasu@gmail.com>
2024-07-03 12:41:52 +0800

    chore: disable relative line numbers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
+-- NOTE: Sync with system clipboard
+-- vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
 vim.opt.clipboard='unnamedplus'
+
+vim.opt.relativenumber = false
 
 vim.o.guifont = "JetBrainsMono Nerd Font:h13" -- text below applies for VimScript
 
