4b96747e5b4e9bd118aa05fa588f8f1602a34307
Erlang Parasu <erlangparasu@gmail.com>
2024-07-03 17:32:03 +0800

    feat: oil plugin: increase lsp timeout

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,11 +4,19 @@ return {
     require('oil').setup({
       keymaps = {
         ["<Esc>"] = "actions.close"
-      }
+      },
+      delete_to_trash = true,
+      lsp_file_methods = {
+        -- Time to wait for LSP file operations to complete before skipping
+        timeout_ms = 60000,
+        -- Set to true to autosave buffers that are updated with LSP willRenameFiles
+        -- Set to "unmodified" to only save unmodified buffers
+        autosave_changes = false,
+      },
     })
   end,
   keys = {
-    { '<leader>fo', '<cmd>Oil<cr>', mode = 'n', desc = "Open Filesystem" },
+    { '<leader>fo', '<cmd>Oil<cr>', mode = 'n', desc = "Open Filesystem (oil)" },
     -- { '-', '<cmd>Oil --float<cr>', mode = 'n', desc = "Open Floating Filesystem" },
   },
 }
