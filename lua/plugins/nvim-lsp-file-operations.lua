03a973598c80fb466fa43bd4f25e00baa0f0c2c9
Erlang Parasu <erlangparasu@gmail.com>
2024-07-03 17:32:44 +0800

    feat: add nvim-lsp-file-operation to support lsp file ops in nvim-tree

---
+++ lua/plugins/nvim-lsp-file-operations.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-tree.lua",
+    },
+    config = function()
+      require("lsp-file-operations").setup {
+        -- used to see debug logs in file `vim.fn.stdpath("cache") .. lsp-file-operations.log`
+        debug = false,
+        -- select which file operations to enable
+        operations = {
+          willRenameFiles = true,
+          didRenameFiles = true,
+          willCreateFiles = true,
+          didCreateFiles = true,
+          willDeleteFiles = true,
+          didDeleteFiles = true,
+        },
+        -- how long to wait (in milliseconds) for file rename information before cancelling
+        timeout_ms = 60000,
+      }
+    end,
+  },
+}
