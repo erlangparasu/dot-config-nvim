fa6e1861da535d09d4ba8cb8a574a5521b285875
Erlang Parasu <erlangparasu@gmail.com>
2024-07-05 10:15:35 +0800

    chore: disable transparent background

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -3,6 +3,13 @@ return {
     "folke/tokyonight.nvim",
     lazy = true,
     -- opts = { style = "storm" },
-    opts = { style = "night" },
+    opts = { 
+      style = "night",
+      -- transparent = true,
+      -- styles = {
+      --   sidebars = "transparent",
+      --   floats = "transparent",
+      -- },
+    },
   }
 }
