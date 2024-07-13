83a0a5ef64b10bb38811058e3bea0f52af9f8b6d
Erlang Parasu <erlangparasu@gmail.com>
2024-07-14 04:26:35 +0800

    chore: screenkey

--- lua/plugins/screenkey.lua
+++ lua/plugins/screenkey.lua
@@ -1,7 +1,7 @@
 return {
   {
     "NStefan002/screenkey.nvim",
-    lazy = true,
-    version = "dev", -- or branch = "dev", to use the latest commit
+    enabled = true,
+    version = "*",
   }
 }
