55abd2c4a59a02ab0d9ae29199c04c671994d4b9
Erlang Parasu <erlangparasu@gmail.com>
2024-07-18 14:28:58 +0800

    feat: tab based scope for Project.nvim

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -4,31 +4,44 @@ return {
     opts = {
       active = true,
 
-      ---@usage set to true to disable setting the current-woriking directory
-      --- Manual mode doesn't automatically change your root directory, so you have
-      --- the option to manually do so using `:ProjectRoot` command.
+      -- Manual mode doesn't automatically change your root directory, so you have
+      -- the option to manually do so using `:ProjectRoot` command.
       manual_mode = true,
 
-      ---@usage Methods of detecting the root directory
-      --- Allowed values: **"lsp"** uses the native neovim lsp
-      --- **"pattern"** uses vim-rooter like glob pattern matching. Here
-      --- order matters: if one is not detected, the other is used as fallback. You
-      --- can also delete or rearangne the detection methods.
-      -- detection_methods = { "lsp", "pattern" }, -- NOTE: lsp detection will get annoying with multiple langs in one project
-      -- detection_methods = { "pattern" },
+      -- Methods of detecting the root directory. **"lsp"** uses the native neovim
+      -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
+      -- order matters: if one is not detected, the other is used as fallback. You
+      -- can also delete or rearangne the detection methods.
+      detection_methods = { "pattern" },
 
-      ---@usage patterns used to detect root dir, when **"pattern"** is in detection_methods
+      -- All the patterns used to detect root dir, when **"pattern"** is in
+      -- detection_methods
       patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "composer.json", "pubspec.yaml", "Cargo.toml" },
 
-      ---@ Show hidden files in telescope when searching for files in a project
+      -- Table of lsp clients to ignore by name
+      -- eg: { "efm", ... }
+      ignore_lsp = {},
+
+      -- Don't calculate root dir on specific directories
+      -- Ex: { "~/.cargo/*", ... }
+      exclude_dirs = {},
+
+      -- Show hidden files in telescope
       show_hidden = false,
 
-      ---@usage When set to false, you will get a message when project.nvim changes your directory.
-      -- When set to false, you will get a message when project.nvim changes your directory.
+      -- When set to false, you will get a message when project.nvim changes your
+      -- directory.
       silent_chdir = false,
 
-      ---@usage list of lsp client names to ignore when using **lsp** detection. eg: { "efm", ... }
-      -- ignore_lsp = {},
+      -- What scope to change the directory, valid options are
+      -- * global (default)
+      -- * tab
+      -- * win
+      scope_chdir = 'tab',
+
+      -- Path where project.nvim will store the project history for use in
+      -- telescope
+      datapath = vim.fn.stdpath("data"),
     },
   }
 }
