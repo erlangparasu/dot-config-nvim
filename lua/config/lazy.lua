777edfece914fe4ced2e09d13fb4616aa1053db9
Erlang Parasu <erlangparasu@gmail.com>
2024-07-21 08:21:40 +0800

    fix: typo

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -48,7 +48,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.svelte" },
-    { import = "lazyvim.plugins.extras.lang.tailwaind" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
