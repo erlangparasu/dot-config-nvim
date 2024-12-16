aed75c2ba2a23a5765f95390792360c76c9882ee
Erlang Parasu <erlangparasu@gmail.com>
2024-12-16 11:50:31 +0800

    feat: Terminal: esc for change to normal mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -11,8 +11,12 @@ local augroup = vim.api.nvim_create_augroup("TerminalBuffer", {})
 vim.api.nvim_create_autocmd({ "BufEnter", "BufAdd", "BufNew", "BufRead", "TermOpen", "TermEnter" }, {
   group = augroup,
   pattern = "term://*",
-  callback = function()
+  callback = function(event)
     vim.opt_local.number = false
+    local buf = event.buf
+    vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to Left Window", buffer = buf, nowait = true })
+    vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to Above Window", buffer = buf, nowait = true })
+    vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode", buffer = buf, nowait = true })
   end,
 })
 
