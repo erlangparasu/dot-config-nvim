3dadbeaa50a07d5a33f84ea08d38af3ecfc8845c
Erlang Parasu <erlangparasu@gmail.com>
2025-01-01 08:58:54 +0800

    chore: cleanup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
 -- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
 -- NOTE: Ref: https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup#open-for-files-and-no-name-buffers
 -- NOTE: Ref: https://neovim.io/doc/user/autocmd#autocmd-events
